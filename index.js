39b826dcd6298703d15ce2426748eb210aafc3ae
EM LU <ell2140@columbia.edu>
2017-02-03 21:42:34 -0500

    Uploading index.js
    Javascript file uploaded

---
+++ index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	//document.ready makes sure the page loads beforehand
+	$('#special').on('click', function(){
+		$('#samopic').html('<img src="http://static.fjcdn.com/pictures/Thank+you+jackie+chan_34b0b5_5572840.jpg">');
+	});
+});
