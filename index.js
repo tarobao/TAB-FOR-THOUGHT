ed5a21c129d99732f6790f996eaf6cdeebe6197b
EM LU <ell2140@columbia.edu>
2017-02-04 11:05:03 -0500

    Deleted code for random background generation
    Code was unused/untested.

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
 $(document).ready(function() {
-
-	function randomHero() {
-	    var heroPics = ['https://s3-us-west-2.amazonaws.com/s.cdpn.io/392/musician-background-1680.jpg','https://s3-us-west-2.amazonaws.com/s.cdpn.io/392/pink-floyd-division-bell-228953.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/392/northstar.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/392/womensrights.jpg'];
-
-	    $('body').css({
-	        'background' : 'url('+ heroPics[Math.floor(Math.random() * heroPics.length)] + ') no-repeat',
-	        'background-attachment' : 'scroll',
-	        'background-position' : '50% 50%',
-	        'background-size' : 'cover'
-	    });
-	}
-
-	// Show Random Image on Page Load
-	randomHero();
-
-	// Simulate a page refresh
-	$(window).on('open', randomHero);
-
 	$.ajax({
 		url : 'https://www.reddit.com/r/showerthoughts.json',
 		type : 'GET',
