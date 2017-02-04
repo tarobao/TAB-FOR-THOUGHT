4c9006d957f0c48ec3ce0c12dd182695970434c3
EM LU <ell2140@columbia.edu>
2017-02-04 07:11:16 -0500

    UPDATED INDEX.JS (FINAL)
    NICE

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 $(document).ready(function() {
-	//document.ready makes sure the page loads beforehand
-	$('#special').on('click', function(){
-		$('#samopic').html('<img src="http://static.fjcdn.com/pictures/Thank+you+jackie+chan_34b0b5_5572840.jpg">');
+	$.ajax({
+		url : 'https://www.reddit.com/r/showerthoughts.json',
+		type : 'GET',
+		success : function(res) {		
+			  	var titles = res.data.children.map(element => {
+					
+					if (element.data.over_18 == false){
+						var textNode = document.createTextNode(element.data.title);
+
+					}
+
+			  		return textNode
+			  	});
+
+			  	var authors = res.data.children.map(element => {
+					
+					if (element.data.over_18 == false){
+						var textNode = document.createTextNode("—" + element.data.author);
+
+					}
+
+			  		return textNode
+			  	});
+
+  				var number = Math.floor((Math.random() * 24) + 1);
+  				if (titles[number].length > 120){
+  					var l = document.getElementsByClassName("long_quote")[0].appendChild(titles[number]);
+  				}
+
+  				else {
+  					var g = document.getElementsByClassName("quote")[0].appendChild(titles[number]);
+
+  				}
+
+  				var a = document.getElementsByClassName("author")[0].appendChild(authors[number]);
+		}
 	});
 });
